a3c8bf1964f7fb31d771997492da585fed75b6d3
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-02 05:12:31 +0530

    added: backend server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const filePath = req.file.path;
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const words = fileContent.match(/\b\w+\b/g);
+  const wordCounts = {};
+
+  for (const word of words) {
+    const lowercaseWord = word.toLowerCase();
+    wordCounts[lowercaseWord] = (wordCounts[lowercaseWord] || 0) + 1;
+  }
+
+  fs.unlinkSync(filePath);
+
+  res.json(wordCounts);
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
