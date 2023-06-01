e9246f33703afa3f9cf4877b918730fd5c2a273b
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-02 02:53:29 +0530

    added: frontend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-
-    HELLO RAJAT
-  </div>;
+  return <div className="App">HELLO RAJAT</div>;
 }
 
 export default App;
