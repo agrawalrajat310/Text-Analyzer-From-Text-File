d3e43323b61149b5406f9f43f4674a3b87cccb40
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-02 06:19:56 +0530

    added: frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
