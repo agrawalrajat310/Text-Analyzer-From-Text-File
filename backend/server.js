cf4fb3bb0b9b39e2f832befff20eee96e9561fa9
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-02 06:28:14 +0530

    added: backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,21 @@
-const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: 'uploads/' });
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
 // Enable CORS for all routes
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post('/upload', upload.single('file'), (req, res) => {
   // Your file upload and word count logic here
 });
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+  console.log('Server is running on port 3001');
 });
