c4f6f0902ccfca4c129465cc7635832c2d9b2a91
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-04 04:23:28 +0530

    added: backend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const cors = require("cors");
 const app = express();
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log("File received:", req.file);
   // Your file upload and word count logic here
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
 });
 
 app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
